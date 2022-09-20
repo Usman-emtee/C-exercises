dd6bede0acb60298f7e32bb155448de031f16eda
Usman Abdullahi <usmanemtee@gmail.com>
2022-09-20 11:24:56 +0000

    My first program in C

---
+++ project1/source.c
@@ -0,0 +1,14 @@
+/* Author: Usman Abdullahi
+ * Purpose: This program prints out a message.
+ * Date: 20-09-2022 
+*/
+#include <stdio.h>
+main()
+{
+ printf("It's fun to write my own program in C");
+ return0;
+}
+
+
+
+
